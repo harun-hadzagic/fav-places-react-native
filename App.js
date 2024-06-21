8165782fc1ba53379717d9cdddfa34072ae0bcca
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-06-21 22:59:46 +0200

    Using native camera to take photos

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import IconButton from './components/ui/IconButton';
+import { Colors } from './constants/colors';
 
 
 const Stack = createNativeStackNavigator()
@@ -14,11 +15,18 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle:{backgroundColor: Colors.primary500},
+          headerTintColor: Colors.gray700,
+          contentStyle: {backgroundColor: Colors.gray700}
+        }}>
           <Stack.Screen name='AllPlaces' component={AllPlaces} options={({navigation})=>({
+            title: 'Your favorite places',
             headerRight: ({tintColor}) => <IconButton icon='add' color={tintColor} size={24} onPress={()=> navigation.navigate('AddPlace')}/>
           })}/>
-          <Stack.Screen name='AddPlace' component={AddPlace}/>
+          <Stack.Screen name='AddPlace' component={AddPlace} options={{
+            title: "Add a new place"
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,11 +1,44 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 const PlaceForm = () => {
+    const [title, setTitle] = useState('')
+
+    const changeTitleHandler = (enteredText) =>{
+        setTitle(enteredText)
+    }
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+        <View>
+            <Text style={styles.lable}>Title</Text>
+            <TextInput style={styles.input} onChange={changeTitleHandler} value={title}/>
+        </View>
+        <ImagePicker />
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+    form:{
+        flex: 1,
+        padding: 24
+    },
+    lable: {
+        fontWeight: 'bold',
+        marginBottom: 4,
+        color: Colors.primary500
+    },
+    input:{
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100
+    }
+})
